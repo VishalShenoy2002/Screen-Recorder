5181ca21cf6af2b2e96a7f77e137d31e010664f2
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-01-09 09:17:56 +0530

    Changed the Style of the Program

--- recorder.py
+++ recorder.py
@@ -3,13 +3,37 @@ import numpy as np
 import pyautogui
 import datetime
 
-FOURCC=cv2.VideoWriter_fourcc("m","p","4","v")
-FPS=12.0
-TIMESTAMP=str(datetime.datetime.now().strftime("%d-%m-%y %H-%M-%S"))
-FILENAME="{}.mp4".format(TIMESTAMP)
-SIZE=(pyautogui.size()[0],pyautogui.size()[1])
+'''
+    TODO:
+        1. Add Different Arguments to run it in a single Command
+        2. Make a Menu Driven Program for the same
+        3. Convert the original program into and Object Called Recorder ---> Done
+'''
 
-video=cv2.VideoWriter(FILENAME,FOURCC,FPS,SIZE)
+class Recorder:
+
+    def __init__(self):
+
+        self.FOURCC=cv2.VideoWriter_fourcc("m","p","4","v")
+        self.FPS=12.0
+        self.TIMESTAMP=str(datetime.datetime.now().strftime("%d-%m-%y %H-%M-%S"))
+        self.FILENAME="{}.mp4".format(TIMESTAMP)
+        self.SIZE=(pyautogui.size()[0],pyautogui.size()[1])
+
+        self.video=cv2.VideoWriter(FILENAME,FOURCC,FPS,SIZE)
+
+    def record():
+        while True:
+            image=pyautogui.screenshot()
+            image=np.array(image)
+            image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            video.write(image)
+            cv2.imshow('Recording',image)
+            if cv2.waitKey(1) & 0xFF==ord('q'):
+                break
+        video.release()
+        cv2.destroyAllWindows()
 
 print('''
    _____                            _____                        _           
@@ -22,14 +46,4 @@ print('''
                                                                              
 ''')
 
-while True:
-    image=pyautogui.screenshot()
-    image=np.array(image)
-    image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    video.write(image)
-    cv2.imshow('Recording',image)
-    if cv2.waitKey(1) & 0xFF==ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
